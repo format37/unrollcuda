9896a4ea216da310635117660c9c9258cabce90f
alex <format37@gmail.com>
2023-02-25 17:36:15 +0400

    modified:   loop_unrolling.py 	modified:   ../../requirements.txt

--- data/scripts/loop_unrolling.py
+++ data/scripts/loop_unrolling.py
@@ -5,15 +5,8 @@ import numpy as np
 import pycuda
 # import mcubes
 # import time
-from datetime import datetime, timedelta
-
-# print pycuda version
-print('PyCUDA version: ', pycuda.VERSION)
-# Print numpy version
-print('Numpy version: ', np.__version__)
-
-# Start time
-start_time = datetime.now()
+from datetime import datetime
+import math
 
 device_id = 0
 drv.init()
@@ -28,21 +21,21 @@ constant_memory = dev.get_attribute(drv.device_attribute.TOTAL_CONSTANT_MEMORY)
 max_block_x = dev.get_attribute(drv.device_attribute.MAX_BLOCK_DIM_X)
 # Max grid x
 max_grid_x = dev.get_attribute(drv.device_attribute.MAX_GRID_DIM_X)
-print('Shared memory per block: ', shared_memory_per_block)
-print('Constant memory: ', constant_memory)
 
+print('[ Max block x: ', max_block_x)
+print('[ Max grid x: ', max_grid_x)
 # Redefine max block x and max grid x
-# max_block_x = 3
-max_grid_x = 100000000
-print('Max block x: ', max_block_x)
-print('Max grid x: ', max_grid_x)
+# max_block_x = 1024
+# max_grid_x = 100000
+print('+ Max block x: ', max_block_x)
+print('+ Max grid x: ', max_grid_x)
 
-shape_0 = 1000
+shape_0 = 10000
 shape_1 = 1000
 shape_2 = 1000
-# steps = 2
 # Define the empty boolean array
 arr = np.zeros(shape_0 * shape_1 * shape_2, dtype=np.bool_)
+arr_comp = np.ones(shape_0 * shape_1 * shape_2, dtype=np.bool_)
 
 # Send grid to gpu
 gpu_arr = gpuarray.to_gpu(arr)
@@ -51,47 +44,29 @@ print('GPU array size: ', gpu_arr.size)
 # Define the grid
 block = (int(max_block_x), 1, 1)
 print('Block: ', block)
-# grid = (int(min(np.ceil(gpu_arr.size/max_block_x),max_grid_x)), 1, 1)
-max_grid_size = int(max_grid_x/max_block_x)
-grid = (int(min(np.ceil(gpu_arr.size/max_block_x),max_grid_size)), 1, 1)
+grid = (int(min(np.ceil(gpu_arr.size/max_block_x),max_grid_x)), 1, 1)
 print('Grid: ', grid)
-
 step = grid[0] * block[0]
 print('Step: ', step)
 
 # Steps count
 steps_count = int(np.ceil(gpu_arr.size / step))
 print('Steps count: ', steps_count)
+
 # Define the kernel function
 kernel_code = """__global__ void test(
         bool *arr,
-        unsigned int shape_0,
-        unsigned int shape_1,
-        unsigned int shape_2,
-        unsigned long long step
+        unsigned long long shape_0,
+        unsigned long long shape_1,
+        unsigned long long shape_2
         )
     {
-        unsigned int idx, x, y, z;
-        for (int s = 0; s < shape_0 * shape_1 * shape_2; s += step) {
-            // arr is 2d array of shape_0 x shape_1
-            idx = threadIdx.x + blockIdx.x * blockDim.x + s;
-            if (idx > shape_0 * shape_1 * shape_2) return;
-            
+        unsigned long long idx = threadIdx.x + blockIdx.x * blockDim.x;
+        while (idx < shape_0 * shape_1 * shape_2) {
             arr[idx] = true;
-            /*
-            // set arr to true if x is 2
-            x = idx % shape_0;
-            if (x == 2) arr[idx] = true;
-            // set arr to true if y is 2
-            y = (idx / shape_0) % shape_1;
-            if (y == 2) arr[idx] = true;
-            // set arr to true if z is 2
-            z = (idx / shape_0 / shape_1) % shape_2;
-            if (z == 2) arr[idx] = true;
-            */
+            idx += blockDim.x * gridDim.x;
         }
     }
-
 /* ### Define the datatypes carefully ###
 Length (bytes) NumPy type	CUDA type
 1 np.int8	    signed char 2**7-1 == 127
@@ -113,10 +88,9 @@ gpu_test = ker.get_function("test")
 print('Calling the kernel...')
 gpu_test(
     gpu_arr,
-    np.uint32(shape_0),
-    np.uint32(shape_1),
-    np.uint32(shape_2),
-    np.uint64(step),
+    np.uint64(shape_0),
+    np.uint64(shape_1),
+    np.uint64(shape_2),
     block=block,
     grid=grid
 )
@@ -125,28 +99,9 @@ arr_new = gpu_arr.get()
 # Clean up
 print('Cleaning up...')
 ctx.pop()
-
+print('Comparing the arrays...')
+ds = arr_new.sum()
+print(ds, ds==arr_new.shape[0])
 # Save as numpy array
-print('Saving as numpy array...')
-np.save('loop_unrolling_3d.npy', arr_new)
-"""
-# Reashape the array to sqrt 2d
-print('Reshaping the array...')
-arr_new = arr_new.reshape((shape_0, shape_1, shape_2), order='F')
-
-# Set the border values to 0
-arr_new[0,:,:] = 0
-arr_new[-1,:,:] = 0
-arr_new[:,0,:] = 0
-arr_new[:,-1,:] = 0
-arr_new[:,:,0] = 0
-arr_new[:,:,-1] = 0
-
-# Save as obj file
-vertices, triangles = mcubes.marching_cubes(arr_new, 0)
-mcubes.export_obj(vertices, triangles, "loop_unrolling_3d.obj")"""
-
-# End time
-end_time = datetime.now()
-
-print('Done in ', end_time - start_time, ' seconds')
+# print('Saving as numpy array...')
+# np.save('../loop_unrolling.npy', arr_new)
