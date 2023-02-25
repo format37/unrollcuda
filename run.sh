2ff1a31f97ad38415aadf4f8a89f0832baf7ec0f
alex <format37@gmail.com>
2023-02-25 14:55:58 +0400

    modified:   ../run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 clear
-sudo docker run -it --rm -v /home/alex/projects/cuda-tips/data:/app/data --gpus all cuda-tips
+sudo docker run -it --rm -v /home/alex/projects/cuda_tips/data:/app/data --gpus all cuda-tips
