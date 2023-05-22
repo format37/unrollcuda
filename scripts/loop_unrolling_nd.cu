1b5e89dd3796a047723a04c18c953573c52abbe3
alex <format37@gmail.com>
2023-05-23 00:54:44 +0400

    modified:   README.md 	modified:   scripts/loop_unrolling_nd.cu

--- scripts/loop_unrolling_nd.cu
+++ scripts/loop_unrolling_nd.cu
@@ -2,7 +2,7 @@ __global__ void loop_unrolling(
     bool *arr,
     unsigned int *shape,
     unsigned long long shape_total,
-    unsigned long long shape_count,
+    unsigned long long dimensions_count,
     unsigned long long step,
     unsigned char order
 )
@@ -10,22 +10,26 @@ __global__ void loop_unrolling(
     unsigned long long idx = threadIdx.x + blockIdx.x * blockDim.x;
     unsigned long long idx_full;
     unsigned int i = 0;
-    unsigned int *indices = new unsigned int[shape_count]; // array to hold the computed indices
+    unsigned int *indices = new unsigned int[dimensions_count]; // array to hold the computed indices
     unsigned long long tmp;
     
     idx_full = i * step + idx;
     while (idx_full < shape_total)
     {
         tmp = idx_full;
-        for (unsigned int j = 0; j < shape_count; ++j)
+        // Compute the indices
+        for (unsigned int j = 0; j < dimensions_count; ++j)
         {
-            unsigned int dimension = (order == 0) ? shape_count - j - 1 : j;
+            unsigned int dimension = (order == 0) ? dimensions_count - j - 1 : j;
+            // Modulo by the dimension size
             indices[dimension] = tmp % shape[dimension];
+            // Divide by the dimension size
             tmp /= shape[dimension];
         }
         // Set true if any index equals to 1
-        for (unsigned int j = 0; j < shape_count; ++j)
+        for (unsigned int j = 0; j < dimensions_count; ++j)
         {
+            // j is the dimension
             if (indices[j] == 1)
             {
                 arr[idx_full] = true;
@@ -36,5 +40,6 @@ __global__ void loop_unrolling(
         i += 1;
         idx_full = i * step + idx;
     }
+    // Free the memory
     delete[] indices;
 }
