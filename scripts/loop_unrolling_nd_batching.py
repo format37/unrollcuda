665d575293ba4964455f3c7d57ffb94aa6f686ce
alex <format37@gmail.com>
2023-05-25 01:06:55 +0400

    deleted:    scripts/loop_unrolling_nd_batching.cu 	modified:   scripts/loop_unrolling_nd_batching.py

--- scripts/loop_unrolling_nd_batching.py
+++ scripts/loop_unrolling_nd_batching.py
@@ -19,97 +19,99 @@ def unroll_cuda(
         reshape_order,
         shape,
         arr,
-        batch_size,
+        memory_batch_size,
         max_block_x,
         max_grid_x,
-        dev,
         ctx
-    ):
+    ):    
 
+    # Define the zeros boolean array
     print('Shape: ', shape)
+    # Print diomensions count
     print('Dimensions count: ', len(shape))
 
+    # TODO: Reshape if needed
+
+    # Send shape to gpu
     gpu_shape = gpuarray.to_gpu(np.array(shape, dtype=np.uint32))
 
+    # Block and grid
     block = (int(max_block_x), 1, 1)
     print('Block: ', block)
+    grid = (int(min(np.ceil(memory_batch_size/max_block_x),max_grid_x)), 1, 1) # Only processing memory_batch_size elements at a time
+    print('Grid: ', grid)
 
-    # Number of iterations required
-    num_iterations = int(np.ceil(len(arr) / float(batch_size)))
-    print(gpu_shape.size, batch_size, 'Number of iterations: ', num_iterations)
+    # Step
+    step = grid[0] * block[0]
+    # print('Step: ', step)
 
-    # Process each batch
-    for i in range(num_iterations):
-        start_idx = i * batch_size
-        end_idx = start_idx + batch_size
+    # Steps count
+    steps_count = int(np.ceil(arr.size / step))
+    print('Steps count: ', steps_count)
 
-        if end_idx > len(arr):
-            end_idx = len(arr)
+    # Read the kernel function from the file
+    with open('loop_unrolling_nd.cu', 'r') as f:
+        kernel_code = f.read()
+    ker = SourceModule(kernel_code)
+    loop_unrolling = ker.get_function("loop_unrolling")
 
-        arr_batch = arr[start_idx:end_idx]
+    for i in range(steps_count):
+        print('Step: ', i)
+        # Determine the size of this batch
+        batch_size = min(step, arr.size - i * step)
 
-        gpu_arr = gpuarray.to_gpu(arr_batch)
-        print('GPU array size: ', gpu_arr.size)
+        # Allocate a smaller array on the device and transfer a batch into it
+        gpu_arr = gpuarray.to_gpu(arr[i * step : i * step + batch_size])
 
-        grid = (int(min(np.ceil(gpu_arr.size/max_block_x),max_grid_x)), 1, 1)
-        print('Grid: ', grid)
-
-        step = grid[0] * block[0]
-        print('Step: ', step)
-
-        steps_count = int(np.ceil(gpu_arr.size / step))
-        print('Steps count: ', steps_count)
-
-        with open('loop_unrolling_nd_batching.cu', 'r') as f:
-            kernel_code = f.read()
-        ker = SourceModule(kernel_code)
-        loop_unrolling = ker.get_function("loop_unrolling")
-
+        # Call the kernel
         loop_unrolling(
             gpu_arr, # arr
             gpu_shape, # shape
             np.uint64(gpu_arr.size), # shape_total
             np.uint64(len(shape)), # shape_count
             np.uint64(step), # step
-            np.uint64(start_idx), # start_idx
             np.uint8(0 if reshape_order=='C' else 1), # unsigned char order
             block=block,
             grid=grid
         )
 
-        arr[start_idx:end_idx] = gpu_arr.get()
+        # Transfer the results back
+        arr[i * step : i * step + batch_size] = gpu_arr.get()
 
+    # Clean up
     ctx.pop()
 
     return arr
 
 
-
 def main():
 
     drv.init()
-    device_id = 0
-    dev = drv.Device(device_id)
+    dev = drv.Device(0)
     ctx = dev.make_context()
 
     reshape_order = 'C'
-        
-    shape = [3,7,4,5,8]
+
+    memory_batch_size = 70
+    shape = [23]
     arr = np.zeros(shape, dtype=np.bool_, order=reshape_order)
-    batch_size = 15
-    # max_block_x = int(int(drv.Device(device_id).get_attribute(drv.device_attribute.MAX_BLOCK_DIM_X)))
+    # Max block x
+    max_block_x = dev.get_attribute(drv.device_attribute.MAX_BLOCK_DIM_X)
+    # Max grid x
+    max_grid_x = dev.get_attribute(drv.device_attribute.MAX_GRID_DIM_X)
+
+    # Redefine max block x
     max_block_x = 8
-    # max_grid_x = int(int(drv.Device(device_id).get_attribute(drv.device_attribute.MAX_GRID_DIM_X)))
-    max_grid_x = 2    
+    # Redefine max grid x
+    max_grid_x = 2
 
     arr_new = unroll_cuda(
         reshape_order,
         shape,
         arr,
-        batch_size,
+        memory_batch_size,
         max_block_x,
         max_grid_x,
-        dev,
         ctx
     )
 
