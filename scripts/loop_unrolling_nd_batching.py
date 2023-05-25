7f123ea75df15a4613780a34983ff0e0528f590e
alex <format37@gmail.com>
2023-05-26 00:30:34 +0400

    modified:   scripts/loop_unrolling_nd_batching.py

--- scripts/loop_unrolling_nd_batching.py
+++ scripts/loop_unrolling_nd_batching.py
@@ -17,53 +17,66 @@ def set_second_to_true(arr):
 
 def unroll_cuda(
         reshape_order,
+        device_id,
         shape,
         arr,
-        memory_batch_size,
-        max_block_x,
-        max_grid_x,
-        ctx
-    ):    
+        memory_batching_size
+    ):
+    drv.init()
+    dev = drv.Device(device_id)
+    ctx = dev.make_context()
+
+    # Max block x
+    max_block_x = dev.get_attribute(drv.device_attribute.MAX_BLOCK_DIM_X)
+    # Max grid x
+    max_grid_x = dev.get_attribute(drv.device_attribute.MAX_GRID_DIM_X)
+
+    # Redefine max block x
+    # max_block_x = 8
+    # Redefine max grid x
+    # max_grid_x = 2
 
     # Define the zeros boolean array
     print('Shape: ', shape)
     # Print diomensions count
     print('Dimensions count: ', len(shape))
 
-    # TODO: Reshape if needed
+    # Reshape to 1d
+    arr_original = arr.reshape(-1, order=reshape_order)
 
-    # Send shape to gpu
-    gpu_shape = gpuarray.to_gpu(np.array(shape, dtype=np.uint32))
+    arr_new = np.empty(0, dtype=np.bool_)  # initialize outside the loop
 
-    # Block and grid
-    block = (int(max_block_x), 1, 1)
-    print('Block: ', block)
-    grid = (int(min(np.ceil(memory_batch_size/max_block_x),max_grid_x)), 1, 1) # Only processing memory_batch_size elements at a time
-    print('Grid: ', grid)
+    # Iterate each batch
+    for batch in range(int(np.ceil(arr_original.size / memory_batching_size))):
+        # Define arr as a part of the original array
+        arr = arr_original[batch*memory_batching_size:(batch+1)*memory_batching_size]
 
-    # Step
-    step = grid[0] * block[0]
-    # print('Step: ', step)
+        # Send array to gpu
+        gpu_arr = gpuarray.to_gpu(arr)
+        print('GPU array size: ', gpu_arr.size)
 
-    # Steps count
-    steps_count = int(np.ceil(arr.size / step))
-    # print('Steps count: ', steps_count)
+        # Send shape to gpu
+        gpu_shape = gpuarray.to_gpu(np.array(shape, dtype=np.uint32))
 
-    # Read the kernel function from the file
-    with open('loop_unrolling_nd.cu', 'r') as f:
-        kernel_code = f.read()
-    ker = SourceModule(kernel_code)
-    loop_unrolling = ker.get_function("loop_unrolling")
+        # Block and grid
+        block = (int(max_block_x), 1, 1)
+        print('Block: ', block)
+        grid = (int(min(np.ceil(gpu_arr.size/max_block_x),max_grid_x)), 1, 1)
+        print('Grid: ', grid)
 
-    for i in range(steps_count):
-        # each 1k elements
-        if i % 1000 == 0:
-            print('Step: ', i, '/', steps_count, end='\r')
-        # Determine the size of this batch
-        batch_size = min(step, arr.size - i * step)
+        # Step
+        step = grid[0] * block[0]
+        print('Step: ', step)
 
-        # Allocate a smaller array on the device and transfer a batch into it
-        gpu_arr = gpuarray.to_gpu(arr[i * step : i * step + batch_size])
+        # Steps count
+        steps_count = int(np.ceil(gpu_arr.size / step))
+        print('Steps count: ', steps_count)
+
+        # Read the kernel function from the file
+        with open('loop_unrolling_nd_batching.cu', 'r') as f:
+            kernel_code = f.read()
+        ker = SourceModule(kernel_code)
+        loop_unrolling = ker.get_function("loop_unrolling")
 
         # Call the kernel
         loop_unrolling(
@@ -72,54 +85,40 @@ def unroll_cuda(
             np.uint64(gpu_arr.size), # shape_total
             np.uint64(len(shape)), # shape_count
             np.uint64(step), # step
-            np.uint8(0 if reshape_order=='C' else 1), # unsigned char order
+            np.uint8(0 if reshape_order=='C' else 1), # unsigned char order,
+            np.uint64(batch), # batch
+            np.uint64(memory_batching_size), # memory_batching_size
             block=block,
             grid=grid
         )
-
-        # Transfer the results back
-        arr[i * step : i * step + batch_size] = gpu_arr.get()
-
+        # Downloading the array...
+        arr_new = np.concatenate((arr_new, gpu_arr.get()))  # concatenate arrays
     # Clean up
     ctx.pop()
 
-    return arr
+    # Reshape to original shape
+    arr_new = arr_new.reshape(shape, order=reshape_order)
+
+    return arr_new
 
 
 def main():
 
-    drv.init()
-    dev = drv.Device(0)
-    ctx = dev.make_context()
-
     reshape_order = 'C'
 
-    shape = [123,234,574]
+    device_id = 0
+    
+    shape = [5,5]
+    memory_batching_size = int(np.prod(shape)/2)
+    print('Memory batching size: ', memory_batching_size)
     arr = np.zeros(shape, dtype=np.bool_, order=reshape_order)
-    # Max block x
-    max_block_x = dev.get_attribute(drv.device_attribute.MAX_BLOCK_DIM_X)
-    # Max grid x
-    max_grid_x = dev.get_attribute(drv.device_attribute.MAX_GRID_DIM_X)
-    # Redefine max block x
-    # max_block_x = 8
-    # Redefine max grid x
-    # max_grid_x = 2
-    # Memory batch size
-    memory_batch_size = max_block_x * 265
-
-    print('Max block x: ', max_block_x)
-    print('Max grid x: ', max_grid_x)
-    print('Memory batch size: ', memory_batch_size)
-    print('Array size: ', arr.size)
 
     arr_new = unroll_cuda(
         reshape_order,
+        device_id,
         shape,
         arr,
-        memory_batch_size,
-        max_block_x,
-        max_grid_x,
-        ctx
+        memory_batching_size
     )
 
     # Prepare the test array
@@ -129,6 +128,9 @@ def main():
 
     # Check the result
     print('Check result: ', np.array_equal(arr_new, arr_test))
+    print('Test: ', arr_test)
+    print('Result: ', arr_new)
+    
 
 
 if __name__ == '__main__':
